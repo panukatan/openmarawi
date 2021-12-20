a4bcd2ff941ed6dd5517d2b294d144f58c71de3d
Ernest Guevarra <ernest@guevarra.io>
2021-12-20 23:27:11 +0000

    remove %>%

--- R/marawi_get.R
+++ R/marawi_get.R
@@ -30,8 +30,7 @@ marawi_ls <- function(id = "1bph1LBRpxwydAvjuggyjmhmq6HhyteY8") {
   googledrive::drive_deauth()
 
   ## Get dribble of files and folders inside Open Marawi Google Drive
-  x <- googledrive::drive_get(id = id) %>%
-    googledrive::drive_ls()
+  x <- googledrive::drive_ls(googledrive::drive_get(id = id))
 
   ## Return x
   x
@@ -71,8 +70,7 @@ marawi_ls_armm <- function(id = marawi_ls()$id[marawi_ls()$name == "ARMM"]) {
   googledrive::drive_deauth()
 
   ## Get dribble of files and folders inside Open Marawi Google Drive
-  x <- googledrive::drive_get(id = id) %>%
-    googledrive::drive_ls()
+  x <- googledrive::drive_ls(googledrive::drive_get(id = id))
 
   ## Return x
   x
